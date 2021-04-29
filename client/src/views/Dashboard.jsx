202fb49135d761ebb95ea87d46d2965df62be067
Prime <doanhthucdn3005@gmail.com>
2021-04-29 15:12:41 +0700

    edit animation

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -76,17 +76,22 @@ const Dashboard = () => {
                 </Row>
 
                 {/* Open Add Post Modal */}
-                {/* <OverlayTrigger
+                <OverlayTrigger
                     placement="left"
                     overlay={<Tooltip>Add a new thing to learn</Tooltip>}
-                > */}
-                <Button
-                    className="btn-floating"
-                    onClick={setShowAddPostModal.bind(this, true)}
                 >
-                    <img src={addIcon} alt="add-post" width="60" height="60" />
-                </Button>
-                {/* </OverlayTrigger> */}
+                    <Button
+                        className="btn-floating"
+                        onClick={setShowAddPostModal.bind(this, true)}
+                    >
+                        <img
+                            src={addIcon}
+                            alt="add-post"
+                            width="60"
+                            height="60"
+                        />
+                    </Button>
+                </OverlayTrigger>
             </>
         );
     }
